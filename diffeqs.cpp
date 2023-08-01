45aa7c0f51b639e21f605124b618b490358c8a72
Walker Xin <walkerxin@outlook.com>
2023-08-01 19:11:34 +0800

    updated diff equs with new metric and design

--- diffeqs.cpp
+++ diffeqs.cpp
@@ -1,141 +1,310 @@
+void christoffel(long double r, long double th, long double christ[4][4][4])
+{
+	long double t2 = pow(r,2);
+	long double t6 = pow(spin,2);
+	long double t25 = pow(spin,4);
+	long double t15 = 2*th;
+	long double t16 = cos(t15);
+	long double t3 = -2 + r;
+	long double t5 = r*t3;
+	long double t7 = t5 + t6;
+	long double t40 = sin(th);
+	long double t41 = pow(t40,2);
+	long double t11 = cos(th);
+	long double t12 = pow(t11,2);
+	long double t50 = t12*t6;
+	long double t51 = t2 + t50;
+	long double t42 = t2 + t6;
+	long double t43 = pow(t42,2);
+	long double t44 = -(t41*t6*t7);
+	long double t45 = t43 + t44;
+	long double t17 = t16*t6;
+	long double t18 = t17 + t2;
+	long double t23 = pow(r,3);
+	long double t19 = 1/t18;
+	long double t46 = 2 + r;
+	long double t47 = r*t46;
+	long double t48 = t47 + t6;
+	long double t29 = 3*r;
+	long double t20 = pow(r,4);
+	long double t21 = -4*t20;
+	long double t22 = -6*t2*t6;
+	long double t24 = -2*t23*t6;
+	long double t26 = -5*r*t25;
+	long double t27 = 3 + r;
+	long double t28 = t2*t27;
+	long double t30 = 1 + t29;
+	long double t31 = t30*t6;
+	long double t32 = t28 + t31;
+	long double t33 = 2*t16*t32*t6;
+	long double t34 = -r;
+	long double t35 = 1 + t34;
+	long double t36 = 4*th;
+	long double t37 = cos(t36);
+	long double t38 = t25*t35*t37;
+	long double t39 = t21 + t22 + t24 + t25 + t26 + t33 + t38;
+	long double t49 = pow(t48,2);
+	long double t52 = pow(t51,3);
+	long double t53 = pow(t40,4);
+	long double t54 = t2*t49*t52*t53*t6;
+	long double t55 = pow(b1,2);
+	long double t56 = pow(t18,2);
+	long double t57 = t55*t56*t6*t7;
+	long double t58 = r*t12*t6;
+	long double t59 = t23 + t58;
+	long double t60 = pow(t59,2);
+	long double t61 = -4*t41*t6;
+	long double t62 = t5 + t6 + t61;
+	long double t63 = -(t60*t62);
+	long double t64 = t57 + t63;
+	long double t65 = t41*t45*t51*t64;
+	long double t66 = t54 + t65;
+	long double t67 = 1/t66;
+	long double t69 = pow(t51,2);
+	long double t93 = t16*t6*t7;
+	long double t8 = b1*r*spin;
+	long double t9 = 1/r;
+	long double t10 = pow(spin,3);
+	long double t13 = b1*t10*t12*t9;
+	long double t14 = t13 + t8;
+	long double t78 = 1 + r;
+	long double t72 = 2*t2;
+	long double t112 = t17 + t6 + t72;
+	long double t146 = pow(r,9);
+	long double t149 = pow(r,10);
+	long double t152 = pow(r,6);
+	long double t155 = pow(r,7);
+	long double t144 = pow(r,8);
+	long double t160 = pow(spin,6);
+	long double t163 = pow(r,5);
+	long double t168 = pow(spin,8);
+	long double t174 = pow(spin,10);
+	long double t182 = -15*r;
+	long double t222 = 4*t55;
+	long double t242 = 6*th;
+	long double t243 = cos(t242);
+	long double t177 = pow(spin,12);
+	long double t255 = 8*th;
+	long double t256 = cos(t255);
+	long double t122 = pow(t40,3);
+	long double t70 = r + t6;
+	long double t71 = 2*t23*t70;
+	long double t73 = t29 + t6 + t72;
+	long double t74 = -2*r*t16*t6*t73;
+	long double t75 = 2*t6;
+	long double t76 = t2 + t29 + t75;
+	long double t77 = -(r*t76);
+	long double t79 = t16*t6*t78;
+	long double t80 = t77 + t79;
+	long double t81 = -2*t12*t6*t80;
+	long double t82 = t71 + t74 + t81;
+	long double t87 = 2*t43*t49;
+	long double t88 = 6 + r;
+	long double t89 = -(t23*t88);
+	long double t90 = 4 + t29;
+	long double t91 = -(r*t6*t90);
+	long double t92 = -2*t25;
+	long double t94 = t89 + t91 + t92 + t93;
+	long double t95 = 2*t20;
+	long double t96 = 2 + t29;
+	long double t97 = r*t6*t96;
+	long double t98 = t25 + t93 + t95 + t97;
+	long double t99 = -(t94*t98);
+	long double t100 = t87 + t99;
+	long double t101 = sin(t15);
+	long double t287 = pow(t18,-3);
+	long double t103 = -6*t23*t6;
+	long double t104 = -3*r*t25;
+	long double t105 = 3*t2;
+	long double t106 = t105 + t6;
+	long double t107 = 2*t106*t16*t6*t78;
+	long double t108 = t25*t37*t78;
+	long double t109 = t103 + t104 + t107 + t108 + t21 + t22 + t25;
+	long double t288 = -(t41*t49*t6);
+	long double t289 = t45*t62;
+	long double t290 = t288 + t289;
+	long double t118 = t34 + t6;
+	long double t124 = t23*t27;
+	long double t125 = 2*r;
+	long double t126 = 1 + t125;
+	long double t127 = r*t126*t6;
+	long double t128 = t124 + t127 + t25;
+	long double t129 = r*t128;
+	long double t130 = -t2;
+	long double t131 = t130 + t6;
+	long double t132 = -(t131*t16*t6);
+	long double t133 = t129 + t132;
+	long double t140 = pow(r,11);
+	long double t142 = pow(r,12);
+	long double t179 = -2*t2;
+	long double t180 = -2 + r + t179;
+	long double t181 = 32*t144*t180;
+	long double t183 = 2 + t182;
+	long double t184 = r*t183;
+	long double t185 = -12*r;
+	long double t186 = 5*t2;
+	long double t187 = 4 + t185 + t186;
+	long double t188 = t187*t55;
+	long double t189 = t184 + t188;
+	long double t190 = 8*t152*t189*t6;
+	long double t191 = -64*t55;
+	long double t192 = -12*t55;
+	long double t193 = 7 + t192;
+	long double t194 = 8*r*t193;
+	long double t195 = 32*t55;
+	long double t196 = -27 + t195;
+	long double t197 = 3*t196*t2;
+	long double t198 = 4 + t191 + t194 + t197;
+	long double t199 = t198*t20*t25;
+	long double t200 = 8 + t182;
+	long double t201 = r*t200;
+	long double t202 = -4*r;
+	long double t203 = 13*t2;
+	long double t204 = 4 + t202 + t203;
+	long double t205 = 3*t204*t55;
+	long double t206 = t201 + t205;
+	long double t207 = 2*t160*t2*t206;
+	long double t208 = -7*r;
+	long double t209 = 6 + t208;
+	long double t210 = -4*t209*t55;
+	long double t211 = -5*r;
+	long double t212 = t210 + t211;
+	long double t213 = r*t168*t212;
+	long double t214 = 6*t174*t55;
+	long double t215 = t181 + t190 + t199 + t207 + t213 + t214;
+	long double t217 = 10*r;
+	long double t218 = -17*t2;
+	long double t219 = -16 + t217 + t218;
+	long double t220 = t152*t219;
+	long double t221 = 8*t55;
+	long double t223 = -15 + t222;
+	long double t224 = t2*t223;
+	long double t225 = 6*r*t55;
+	long double t226 = r + t225;
+	long double t227 = -2*t226;
+	long double t228 = -2 + t221 + t224 + t227;
+	long double t229 = 2*t20*t228*t6;
+	long double t230 = 8*t2;
+	long double t231 = 12*r;
+	long double t232 = -9*t2;
+	long double t233 = 4 + t231 + t232;
+	long double t234 = t233*t55;
+	long double t235 = t230 + t234;
+	long double t236 = -2*t2*t235*t25;
+	long double t237 = -1 + t222;
+	long double t238 = 3*t160*t2*t237;
+	long double t239 = 2*t168*t55;
+	long double t240 = t220 + t229 + t236 + t238 + t239;
+	long double t263 = 16*t155;
+	long double t264 = -8*t20*t6;
+	long double t265 = -18*t2*t25;
+	long double t266 = -6*t23*t25;
+	long double t267 = 6*r;
+	long double t268 = 1 + t267;
+	long double t269 = 8*t20*t268;
+	long double t270 = 5*r;
+	long double t271 = 3 + t270;
+	long double t272 = 8*t2*t271*t6;
+	long double t273 = 7*r;
+	long double t274 = 1 + t273;
+	long double t275 = t25*t274;
+	long double t276 = t269 + t272 + t275;
+	long double t277 = t16*t276*t6;
+	long double t278 = -3*r;
+	long double t279 = 7*t2;
+	long double t280 = 4*t6;
+	long double t281 = t278 + t279 + t280;
+	long double t282 = 2*r*t25*t281*t37;
+	long double t283 = -(t160*t243);
+	long double t284 = r*t160*t243;
+	long double t285 = t263 + t264 + t265 + t266 + t277 + t282 + t283 + t284;
+	long double t361 = 1/t51;
+	long double t111 = 1/t7;
+	long double t113 = -3 + r;
+	long double t139 = pow(t112,2);
+	
+	christ[0][0][0] = (t14*t19*t2*t39*t41*t45*t67*t7)/4.;
+	christ[0][0][1] = -0.5*(t19*t2*t41*t67*t69*(-0.5*(t39*t45) + t41*t48*t6*t82));
+	christ[0][0][2] = (t100*t101*t19*t2*t41*t6*t67*t69)/4.;
+	christ[0][0][3] = (spin*t109*t14*t19*t2*t45*t53*t67*t7)/4.;
+	christ[0][1][1] = (b1*spin*t111*t112*t41*t45*t56*(2*t113*t23 + t25 + r*(-1 + 4*r)*t6 + t118*t16*t6)*t67)/4.;
+	christ[0][1][2] = -(t11*t122*t14*t2*t45*t56*t6*t67);
+	christ[0][1][3] = -0.5*(spin*t112*t133*t53*t60*t67);
+	christ[0][2][2] = -(t14*t23*t41*t45*t56*t67*t7);
+	christ[0][2][3] = (8*t10*t11*t122*t139*t2*(-4*t2 + t20 + t25 + 2*t2*t6))/(64*t140 - 32*t142 - 24*t152*t160 + 144*t160*t163 - 3*t174*t2 + 16*t160*t20 - 15*t168*t20 + 30*t168*t23 - 14*t152*t160*t243 + 16*t160*t163*t243 - 6*t174*t2*t243 - 8*t160*t20*t243 - 20*t168*t20*t243 - 12*t144*t25 + 64*t152*t25 + 312*t155*t25 - t174*t2*t256 - t168*t20*t256 + 2*t168*t23*t256 + 4*t240*t25*t37 + 96*t152*t160*t55 - 96*t160*t163*t55 + 8*t177*t55 - 32*t168*t2*t55 + 48*t174*t2*t55 + 64*t160*t20*t55 + 88*t168*t20*t55 - 96*t168*t23*t55 - 16*r*t174*t243*t55 + 4*t177*t243*t55 + 16*t168*t2*t243*t55 + 8*t174*t2*t243*t55 + 4*t168*t20*t243*t55 - 16*t168*t23*t243*t55 + 80*t144*t25*t55 - 64*t152*t25*t55 - 64*t155*t25*t55 + 128*t144*t6 + 192*t146*t6 - 32*t149*t6 + 2*t16*t215*t6 - 64*t146*t55*t6 + 32*t149*t55*t6);
+	christ[0][3][3] = -0.0625*(t14*t19*t2*t285*t45*t53*t67*t7);
+
+	christ[1][0][0] = -0.25*(t2*t287*t290*t39*t41*t67*t69*t7);
+	christ[1][0][1] = -0.5*(t14*t19*t2*t41*t67*t7*((t39*t45)/2. - t41*t48*t6*t82));
+	christ[1][0][2] = -0.25*(t100*t101*t14*t19*t2*t41*t6*t67*t7);
+	christ[1][0][3] = -0.25*(spin*t109*t287*t290*t53*t60*t67*t7);
+	christ[1][1][1] = -0.5*(t2*t41*t51*t67*((2*t45*t55*t56*t6)/pow(r,3) - (t51*(2*r*t118 + 2*t12*t35*t6)*(t41*t49*t6 - t45*t62))/pow(t7,2))*t7);
+	christ[1][1][2] = t11*t122*t2*t290*t6*t67*t69;
+	christ[1][1][3] = (spin*t112*t133*t14*t2*t53*t67*t7)/2.;
+	christ[1][2][2] = t23*t290*t41*t67*t69*t7;
+	christ[1][2][3] = (32*b1*r*t11*t122*t25*t48*t51*pow(t7,2))/(-64*t140 + 32*t142 + 24*t152*t160 - 144*t160*t163 + 3*t174*t2 - 16*t160*t20 + 15*t168*t20 - 30*t168*t23 + 14*t152*t160*t243 - 16*t160*t163*t243 + 6*t174*t2*t243 + 8*t160*t20*t243 + 20*t168*t20*t243 + 12*t144*t25 - 64*t152*t25 - 312*t155*t25 + t174*t2*t256 + t168*t20*t256 - 2*t168*t23*t256 - 4*t240*t25*t37 - 96*t152*t160*t55 + 96*t160*t163*t55 - 8*t177*t55 + 32*t168*t2*t55 - 48*t174*t2*t55 - 64*t160*t20*t55 - 88*t168*t20*t55 + 96*t168*t23*t55 + 16*r*t174*t243*t55 - 4*t177*t243*t55 - 16*t168*t2*t243*t55 - 8*t174*t2*t243*t55 - 4*t168*t20*t243*t55 + 16*t168*t23*t243*t55 - 80*t144*t25*t55 + 64*t152*t25*t55 + 64*t155*t25*t55 - 128*t144*t6 - 192*t146*t6 + 32*t149*t6 - 2*t16*t215*t6 + 64*t146*t55*t6 - 32*t149*t55*t6);
+	christ[1][3][3] = (t285*t287*t290*t53*t60*t67*t7)/16.;
+
+	christ[2][0][0] = -0.5*(t101*t287*t361*t6*t94);
+	christ[2][0][1] = 0;
+	christ[2][0][2] = 0;
+	christ[2][0][3] = (spin*t101*t287*t361*t43*t48)/2.;
+	christ[2][1][1] = t11*t111*t361*t40*t6;
+	christ[2][1][2] = r*t361;
+	christ[2][1][3] = 0;
+	christ[2][2][2] = -(t11*t361*t40*t6);
+	christ[2][2][3] = 0;
+	christ[2][3][3] = t287*t40*(-(t11*t18*t45) - 2*t101*t40*t45*t6 + t11*t18*t361*t41*t45*t6 + t11*t18*t41*t6*t7);
+	
+	christ[3][0][0] = (b1*r*t19*t39*t41*t48*t51*t6*t67*t7)/4.;
+	christ[3][0][1] = -0.25*(spin*t19*t41*t67*(-(t39*t48*t60) - 2*t64*t82));
+	christ[3][0][2] = (spin*t101*t48*t67*t7*(t16*(t160*(15 + t191)*t2 + 16*t20*t25*(3 - 2*t55) - 32*t168*t55 + 48*t152*t6) + t2*(32*t152 + 10*t160 + t160*t243 + 36*t2*t25 - 32*t160*t55 - 64*t2*t25*t55 + 48*t20*t6 - 32*t20*t55*t6 + 6*t25*t37*(t6 + t72))))/64.;
+	christ[3][0][3] = (b1*r*t10*t109*t19*t48*t51*t53*t67*t7)/4.;
+	christ[3][1][1] = (b1*t111*pow(t18,4)*t2*t41*t48*t51*t6*t67*(t118*t12*t6 + t2*(r*t113 + t75)))/pow(t23 + r*t16*t6,2);
+	christ[3][1][2] = -(b1*r*t11*t122*t25*t48*t51*t56*t67);
+	christ[3][1][3] = -0.5*(t19*t67*(-(t64*(2*r*t41*(t2*(t20 - t25 + t118*t41*t6) + t16*(t160 + 4*t2*t25 + 3*t20*t6 - t25*t41*(t278 + t6 + t72))) - 2*t12*t25*t53*(-(t16*t35*t6) + r*(t75 + 3*r*t78)))) + t2*t48*t53*t6*t69*t82));
+	christ[3][2][2] = -(b1*t2*t41*t48*t51*t56*t6*t67*t7);
+	christ[3][2][3] = t19*t67*((t11*t122*t139*t2*t43*t49*t6)/4. - t40*t64*(-(t11*t18*t45*t51) + t11*t18*t41*t45*t6 - 2*t101*t40*t45*t51*t6 + t11*t18*t41*t51*t6*t7));
+	christ[3][3][3] = -0.0625*(b1*r*t19*t285*t48*t51*t53*t6*t67*t7);
+};
+
 void diffeqs(long double b, long double vars[], long double diffs[])
 {
-	long double r, th;
-	long double kt, kphi;
-	long double kt2, kr2, kth2, kp2, ktp, krth;
-	long double ch_rtt, ch_rtp, ch_rrr, ch_rrth, ch_rthth, ch_rpp;
-	long double ch_thtt, ch_thtp, ch_thrr, ch_thrth, ch_ththth, ch_thpp;
-	long double gtt, gtp, gpp, gurr, guthth;
-	long double dgttdr, dgttdth, dgtpdr, dgtpdth, dgrrdr, dgrrdth, dgththdr, dgththdth, dgppdr, dgppdth;
-	long double hgurr, hguthth;
-	long double denom;
-	long double t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21, t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32, t33, t34, t35, t36, t37, t38, t39, t40, t41, t42, t43, t44, t45, t46, t47, t48, t49, t50, t51, t52;
+	long double t, r, th, phi;
+	long double dt, dr, dth, dphi;
+	long double christ[4][4][4];
 
-	r = vars[0];
-	th = vars[1];
+	t = vars[0];
+	r = vars[1];
+	th = vars[2];
+	phi = vars[3];
 
-	/* Calculate necessary metric components and derivatives of metric components */
-	/* Use code generation in Maple or Mathematica to optimize, if possible */
+	dt = vars[4];
+	dr = vars[5];
+	dth = vars[6];
+	dphi = vars[7];
 
-	t1 = r * r;
-	t2 = pow(t1, 0.2e1);
-	t3 = r * t1;
-	t4 = t1 * t2;
-	t5 = r * t2;
-	t6 = a22 + t1;
-	t7 = sin(th);
-	t8 = spin * spin;
-	t9 = pow(t6, 0.2e1);
-	t10 = pow(t7, 0.2e1);
-	t11 = 2;
-	t12 = t5 * (double) t11 - t8 * (-t10 * t9 + t2) - t4;
-	t13 = cos(th);
-	t14 = t8 * pow(t13, 0.2e1);
-	t15 = (t14 + t1) * r + epsi3;
-	t16 = t8 + t1;
-	t17 = a13 + t3;
-	t18 = t10 * t8;
-	t19 = t18 * r;
-	t20 = t19 * t6;
-	t21 = t16 * t17;
-	t22 = -t21 + t20;
-	t17 = t10 * ((double) t11 * t3 * t2 * t8 - t4 * t8 * t16) + pow(t16, 0.2e1) * pow(t17, 0.2e1);
-	t23 = 0.1e1 / r;
-	t24 = pow(t23, 0.2e1);
-	t25 = t23 * t24;
-	t26 = a13 * t25 + 0.1e1;
-	t27 = a22 * t24 + 0.1e1;
-	t28 = t16 * t26;
-	t29 = (double) t11 * r;
-	t30 = epsi3 * t23 + t1 + t14;
-	t31 = -t18 * t27 + t28;
-	t32 = t16 - t29;
-	t33 = a52 * t24 + 0.1e1;
-	t34 = 0.3e1 * t1;
-	t35 = t14 + t34;
-	t36 = (0.5e1 / 0.2e1 * t1 + 0.3e1 / 0.2e1 * t8) * r + a13;
-	t34 = r * t36 - t18 * (a22 + t34) / 0.2e1;
-	t22 = 0.1e1 / t22;
-	t37 = pow(t22, 0.2e1);
-	t38 = t22 * t37;
-	t39 = t15 * r * t37;
-	t40 = a52 * t8;
-	t41 = a52 + t8;
-	t42 = a52 + t1;
-	t43 = -epsi3 * t24 + t29;
-	t44 = t10 * t15 * t37;
-	t45 = t15 * t23;
-	t46 = a22 + t8;
-	t47 = t8 * a22;
-	t31 = 0.1e1 / t31;
-	t27 = (t28 * t27 - t1 + t29 - t8) * pow(t31, 0.2e1);
-	t28 = t10 * spin;
-	t5 = -(double) t11 * t7 * t17 * t37 * t13 * (t18 - t45) - 0.4e1 * t7 * t10 * t15 * t37 * t8 * t13 * (t5 * t32 / 0.2e1 + t17 * t22 * t6);
-	t29 = t8 * a13;
-	t48 = 0.1e1 / t30;
-	t49 = 0.1e1 / t33;
-	t50 = -0.1e1 / t32;
-	t51 = 0.1e1 / t32;
-	t42 = pow(t42, -0.2e1);
-	t52 = (double) t11 * t8 * t13 * t7;
-	t40 = (-(double) t11 * (t2 * (-t41 + r) - t14 * ((t1 * (-r + 0.1e1) - a52) * r + t40)) * r + t3 * (0.4e1 * epsi3 + 0.4e1 * t40 + (-0.6e1 * a52 - 0.3e1 * epsi3) * r) + epsi3 * (-t1 * t41 + t40)) * pow(t50, 0.2e1) * t42;
-	t4 = (double) t11 * t13 * (t21 * t15 + t19 * (a22 * epsi3 - ((-(a22 - t8) * r - epsi3 + a13) * r - t14 * t6) * r - t29 + t20)) * ((double) t11 * t4 + t1 * (a13 * t46 + ((a22 * r + a13) * r + t47) * r) + t29 * a22) * t7 * spin * t38;
-
-	gtt = t39 * t12;
-	gpp = t44 * t17 * t23;
-	gtp = -t28 * t27 * t30;
-
-	gurr = t48 * t32 * t33;
-	guthth = t48;
-
-	dgttdr = 0.4e1 * t39 * (r * (t3 * (-0.3e1 / 0.2e1 * r + 0.5e1 / 0.2e1) - t8 * (-t10 * t6 + t1)) + t34 * t12 * t22) + t12 * t37 * (r * t35 + t15);
-	dgrrdr = t40;
-	dgththdr = t43;
-	dgppdr = 0.4e1 * t44 * (t21 * t36 + 0.7e1 / 0.2e1 * t18 * ((-0.4e1 / 0.7e1 * r + 0.1e1) * r - 0.3e1 / 0.7e1 * t8) * t2 + t17 * t34 * t22 * t23) + t10 * t17 * t37 * t23 * (t35 - t45);
-	dgtpdr = t28 * (t27 * ((double) t11 * t30 * t31 * ((double) t11 * (t18 * a22 * t25 + r * t26) - 0.3e1 * t16 * a13 * pow(t24, 0.2e1)) - t43) + 0.5e1 * t45 * (a13 * (t1 * (0.3e1 / 0.5e1 * t46 + t1 / 0.5e1) + t47) - 0.2e1 / 0.5e1 * t3 * (-t47 + t3)) * t37);
-
-	dgttdth = (double) t11 * t8 * r * t13 * t7 * t37 * (-r * t12 + t15 * t9) - 0.4e1 * t12 * t1 * t15 * t38 * t8 * t6 * t7 * t13;
-	dgrrdth = -t52 * t51 * t49;
-	dgththdth = -t52;
-	dgppdth = t5;
-	dgtpdth = t4;
-
-	hgurr = 0.5*gurr;
-	hguthth = 0.5*guthth;
-
-	/* Christoffel symbols */
-	ch_rtt = -hgurr*dgttdr;
-	ch_rtp = -hgurr*dgtpdr;
-	ch_rrr = hgurr*dgrrdr;
-	ch_rrth = hgurr*dgrrdth;
-	ch_rthth = -hgurr*dgththdr;
-	ch_rpp = -hgurr*dgppdr;
-	ch_thtt = -hguthth*dgttdth;
-	ch_thtp = -hguthth*dgtpdth;
-	ch_thrr = -hguthth*dgrrdth;
-	ch_thrth = hguthth*dgththdr;
-	ch_ththth = hguthth*dgththdth;
-	ch_thpp = -hguthth*dgppdth;
-
-	denom = (gtt*gpp-gtp*gtp); // common denominator in Eq (26) and Eq (27) in Public Release
-
-	/* t and phi photon 4-momentum */
-	kt = -(gpp+b*gtp)/denom; // c.f Eq (26)
-	kphi = (gtp+b*gtt)/denom; // c.f Eq (27)
-
-	kt2 = kt*kt;
-	kr2 = vars[3]*vars[3];
-	kth2 = vars[4]*vars[4];
-	kp2 = kphi*kphi;
-	ktp = kt*kphi;
-	krth = vars[3]*vars[4];
-
-	/* 1st order diff eqs for r, theta and phi */
-	diffs[0] = vars[3]; // kr
-	diffs[1] = vars[4]; // kth
-	diffs[2] = kphi;
+	/* 1st order diff eqs */
+	diffs[0] = dt; // dt
+	diffs[1] = dr; // dr
+	diffs[2] = dth; // dth
+	diffs[3] = dphi; // dphi
 
 	/* 2nd order diff eqs for r and theta; c.f. Eq (28) and Eq (29) */
-	diffs[3] = -(ch_rtt*kt2+ch_rrr*kr2+ch_rthth*kth2+ch_rpp*kp2+2.0*(ch_rtp*ktp+ch_rrth*krth));
-	diffs[4] = -(ch_thtt*kt2+ch_thrr*kr2+ch_ththth*kth2+ch_thpp*kp2+2.0*(ch_thtp*ktp+ch_thrth*krth));
+	diffs[4] = -christ[0][0][0]*dt*dt - 2.0*christ[0][0][1]*dt*dr - 2.0*christ[0][0][2]*dt*dth - 2.0*christ[0][0][3]*dt*dphi 
+			   -christ[0][1][1]*dr*dr - 2.0*christ[0][1][2]*dr*dth - 2.0*christ[0][1][3]*dr*dphi 
+			   -christ[0][2][2]*dth*dth - 2.0*christ[0][2][3]*dth*dphi 
+			   -christ[0][3][3]*dphi*dphi; // d2t
+	diffs[5] = -christ[1][0][0]*dt*dt - 2.0*christ[1][0][1]*dt*dr - 2.0*christ[1][0][2]*dt*dth - 2.0*christ[1][0][3]*dt*dphi 
+			   -christ[1][1][1]*dr*dr - 2.0*christ[1][1][2]*dr*dth - 2.0*christ[1][1][3]*dr*dphi 
+			   -christ[1][2][2]*dth*dth - 2.0*christ[1][2][3]*dth*dphi 
+			   -christ[1][3][3]*dphi*dphi; // d2r
+	diffs[6] = -christ[2][0][0]*dt*dt - 2.0*christ[2][0][1]*dt*dr - 2.0*christ[2][0][2]*dt*dth - 2.0*christ[2][0][3]*dt*dphi 
+			   -christ[2][1][1]*dr*dr - 2.0*christ[2][1][2]*dr*dth - 2.0*christ[2][1][3]*dr*dphi 
+			   -christ[2][2][2]*dth*dth - 2.0*christ[2][2][3]*dth*dphi 
+			   -christ[2][3][3]*dphi*dphi; // d2th
+	diffs[7] = -christ[3][0][0]*dt*dt - 2.0*christ[3][0][1]*dt*dr - 2.0*christ[3][0][2]*dt*dth - 2.0*christ[3][0][3]*dt*dphi 
+			   -christ[3][1][1]*dr*dr - 2.0*christ[3][1][2]*dr*dth - 2.0*christ[3][1][3]*dr*dphi 
+			   -christ[3][2][2]*dth*dth - 2.0*christ[3][2][3]*dth*dphi 
+			   -christ[3][3][3]*dphi*dphi; // d2phi
 }
