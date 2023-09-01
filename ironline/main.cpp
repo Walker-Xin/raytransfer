914b3c106cc29ada4628dcb45f4bc8dd732f73db
Walker Xin <walkerxin@outlook.com>
2023-09-01 21:16:25 +0800

    c1 deformation update

--- ironline/main.cpp
+++ ironline/main.cpp
@@ -30,9 +30,12 @@ int main(int argc, char *argv[])
 	int stop_integration;
 	int n1, n2, n3;
 	int i, j, m;
+	int n_robs, i_robs;
 
 	char filename_i[128];
 	char filename_o[128];
+
+	double time_taken, iteration_time, expected_time;
 
 	FILE *finput;
 	FILE *foutput;
@@ -41,13 +44,13 @@ int main(int argc, char *argv[])
 	FILE *fgoal;
 
 	/* ----- Set computation parameters ----- */
-	spin = 0.9; // spin parameter
+	spin = 0.5; // spin parameter
 	spin2 = spin * spin;
-	defpar = 0.2; // deformation parameter
+	defpar = 0.05; // deformation parameter
 	iobs_deg = 45.0; // inclination angle of the observer in degrees
 	dobs = 1.0e+8;
 	inc = Pi / 180 * iobs_deg; // inclination angle of the observer in rad
-	errtol = 1.0e-5; // error tolerance for RK45
+	errtol = 1.0e-8; // error tolerance for RK45
 
 	/* Set free parameters from user input if provided */
 	if (argc > 1)
@@ -58,7 +61,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("Using default parameters: spin = %f, defpar = %f, iobs_deg = %f\n", spin, defpar, iobs_deg);
+		printf("Using default parameters. spin = %f, deformation = %f, inclination = %f, error tolerence = %e\n", spin, defpar, iobs_deg, errtol);
 	}
 
 	D = 10.0; /* distance Earth-binary system in kpc */
@@ -75,6 +78,7 @@ int main(int argc, char *argv[])
 
 	isco = find_isco(spin, defpar);
 	// isco = 4.2330000000134405; // set maunally
+	printf("Innermost stable circular orbit: %f\n", isco);
 
 	rin = isco;			 /* inner radius of the accretion disk; set isco */
 	rout = isco + 250.0; /* outer radius of the accretion disk */
@@ -108,14 +112,33 @@ int main(int argc, char *argv[])
 	sprintf(filename_o, "iron_a%.03f.def%.02f.i%.02f.dat", spin, defpar, iobs_deg);
 
 	// Start timer
-	clock_t start, end;
+	clock_t start, end, mid;
 	start = clock();
+	mid = clock();
+
+	// Calculate number of robs
+	n_robs = 0;
+	i_robs = 0;
+	for (robs = robs_i; robs < robs_f; robs = robs * rstep)
+	{
+		n_robs++;
+	}
 
 	/* ----- assign photon position in the grid ----- */
 
 	for (robs = robs_i; robs < robs_f; robs = robs * rstep)
 	{
-		printf("Now at robs = %f \n", robs);
+		i_robs++;
+
+		// update progress for every 10 robs
+		if (i_robs % 10 == 0)
+		{
+			// calculate expected time usage
+			iteration_time = double(clock() - mid) / double(CLOCKS_PER_SEC);
+			mid = clock();
+			expected_time = iteration_time * (n_robs - i_robs)/10.0;
+			printf("i_robs = %i; robs = %f; expected time left: %f minutes\n", i_robs, robs, expected_time/60.0);
+		}
 
 		for (i = 0; i <= imax - 1; i++)
 			fphi[i] = 0;
@@ -169,7 +192,7 @@ int main(int argc, char *argv[])
 	
 	// End timer and print time taken in miniutes
 	end = clock();
-	double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+	time_taken = double(end - start) / double(CLOCKS_PER_SEC);
 	printf("Time taken by program is : %f minutes\n", time_taken / 60.0);
 
 	/* --- print spectrum --- */
